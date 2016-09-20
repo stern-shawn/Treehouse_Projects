5c2370c4fed527d1c7b9d301c09a7e8154ec7e7f
stern-shawn <stern.shawn@gmail.com>
2016-09-19 20:34:39 -0700

    Added search functionality so that user can type a desired username into the field and be redirected to the path with results (if there are no errors)

--- ProfileSearcher_NodeJS/router.js
+++ ProfileSearcher_NodeJS/router.js
@@ -1,24 +1,36 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
+var querystring = require("querystring");
 
 // Set output type to html so output is rendered instead of output as plain text
 var commonHeaders = {'Content-Type': 'text/html'};
 
 // Handle the http route GET / and POST / ie HOME
 function home(request, response) {
-// If url === / && GET
-  // show the serach field
+  // If url === / && GET
+  // show the search field
   if (request.url === "/") {
-    response.writeHead(200, commonHeaders);
-    renderer.view("header", {}, response);
-    renderer.view("search", {}, response);
-    renderer.view("footer", {}, response);
-    response.end();
+    if (request.method.toLowerCase() === "get") {
+      response.writeHead(200, commonHeaders);
+      renderer.view("header", {}, response);
+      renderer.view("search", {}, response);
+      renderer.view("footer", {}, response);
+      response.end();
+    } else {
+      // If url === / && POST from the submit button
+
+      // Get post data from body
+      request.on("data", function(postBody) {
+        // extract username
+        var query = querystring.parse(postBody.toString());
+
+        // redirect to site/username using Location header,
+        // Code303 forces a GET on a POST request
+        response.writeHead(303, {"Location": "/" + query.username});
+        response.end();
+      })
+    }
   }
-
-
-// If url === / && POST
-  // redirect to site/username
 }
 
 // Handle HTTP route Get /:userame ie /chalkers or /shawnste
@@ -27,16 +39,13 @@ function user(request, response) {
   // Remove the slash from url and check if we have any username input
   var username = request.url.replace("/", "");
   if (username.length > 0) {
-
     response.writeHead(200, commonHeaders);
     renderer.view("header", {}, response);
 
-     // get *'s json from treehouse
+    // get *'s json from treehouse
     var studentProfile = new Profile(username);
     // on end, show profile
     studentProfile.on("end", function(profileJSON){
-      // Show profile
-
       // Store values that we need
       var values = {
         avatarUrl: profileJSON.gravatar_url,
