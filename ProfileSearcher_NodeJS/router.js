caec042fee58b7fb71b9e6ad2839347ea06b7f82
stern-shawn <stern.shawn@gmail.com>
2016-09-19 18:02:51 -0700

    Adding Profile import to router.js so that the username route can grab the given username, take relevant values from the server response, and print to the page. Including error handling in case user searches for a nonexistant username, etc.

--- ProfileSearcher_NodeJS/router.js
+++ ProfileSearcher_NodeJS/router.js
@@ -1,5 +1,6 @@
-// Handle the http route GET / and POST / ie HOME
+var Profile = require("./profile.js");
 
+// Handle the http route GET / and POST / ie HOME
 function home(request, response) {
 // If url === / && GET
   // show the serach field
@@ -9,7 +10,6 @@ function home(request, response) {
     response.write("Search\n");
     response.end("Footer\n");
   }
-
 
 // If url === / && POST
   // redirect to site/username
@@ -21,15 +21,33 @@ function user(request, response) {
   // Remove the slash from url and check if we have any username input
   var username = request.url.replace("/", "");
   if (username.length > 0) {
-    
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("Header\n");
-    response.write("Search for " + username + "\n");
-    response.end("Footer\n");
-    
-  // get *'s json from treehouse
+
+     // get *'s json from treehouse
+    var studentProfile = new Profile(username);
     // on end, show profile
+    studentProfile.on("end", function(profileJSON){
+      // Show profile
+
+      // Store values that we need
+      var values = {
+        avatarUrl: profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript
+      }
+
+      // Simple response
+      response.write(values.username + " has " + values.badges + " badges\n");
+      response.end("Footer\n");
+    });
+
     // on error, show error
+    studentProfile.on("error", function(error) {
+      response.write(error.message + "\n");
+      response.end("Footer\n");
+    });
   }
 }
 
