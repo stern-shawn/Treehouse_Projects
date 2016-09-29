2cf3fef98c469583ffc6adf378a12882a4479bcf
stern-shawn <stern.shawn@gmail.com>
2016-09-28 22:12:10 -0700

    Added mock blog content as a json file, and modified the blog/ route of the app to accept a title parameter so users can specify a particular post's content to have returned

--- express-basics/src/app.js
+++ express-basics/src/app.js
@@ -1,13 +1,23 @@
 'use strict'
 
 // Import express module
-var express = require('express')
+var express = require('express'),
+      posts = require('./mock/posts.json')
 var app = express()
 
 // Set up route for initial visit to the site
-app.get('/', function (request, response) {
-  response.send('Just getting started!')
+app.get('/', function (req, res) {
+  res.send('<h1>Just getting started! App mastery loading...</h1>')
+})
+
+// Route for our blog site
+app.get('/blog/:title', function (req, res) {
+  var title = req.params.title
+  var post = posts[title]
+  res.send(post)
 })
 
 // Set up the server (port 3000)
-app.listen(3000)
+app.listen(3000, function () {
+  console.log('Front-end server running on port 3000')
+})
