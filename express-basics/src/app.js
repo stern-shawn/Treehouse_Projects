1c0ac2f891255de74ea2c02e8bb47e744320e01a
stern-shawn <stern.shawn@gmail.com>
2016-09-28 21:32:08 -0700

    Updating app.js to include the basic route and response for the root of the app

--- express-basics/src/app.js
+++ express-basics/src/app.js
@@ -1,3 +1,13 @@
-'use strict';
+'use strict'
 
-var express = require('express');
+// Import express module
+var express = require('express')
+var app = express()
+
+// Set up route for initial visit to the site
+app.get('/', function (request, response) {
+  response.send('Just getting started!')
+})
+
+// Set up the server (port 3000)
+app.listen(3000)
