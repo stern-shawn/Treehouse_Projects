cbf86141ef42981eb19341d9d0a6720e707a5e0f
stern-shawn <stern.shawn@gmail.com>
2016-10-01 12:20:09 -0700

    Updating project to use Jade template for the index. 503 error code and placeholder text added to blog path root in case no post is specified.

--- express-basics/src/app.js
+++ express-basics/src/app.js
@@ -5,16 +5,27 @@ var express = require('express'),
       posts = require('./mock/posts.json')
 var app = express()
 
+// Set view engine to Jade and direct to src/templates w/ relative path
+app.set('view engine', 'jade')
+app.set('views', __dirname + '/templates')
+
 // Set up route for initial visit to the site
 app.get('/', function (req, res) {
-  res.send('<h1>Just getting started! App mastery loading...</h1>')
+  // We can pass index instead of index.jade since engine is set to Jade
+  res.render('index')
 })
 
-// Route for our blog site
-app.get('/blog/:title', function (req, res) {
+// Route for our blog site. Use question mark to make param optional
+app.get('/blog/:title?', function (req, res) {
   var title = req.params.title
-  var post = posts[title]
-  res.send(post)
+  if (title === undefined) {
+    // 503 to indicate to search engines under construction
+    res.status(503)
+    res.send('Page under construction!')
+  } else {
+    var post = posts[title]
+    res.send(post)
+  }
 })
 
 // Set up the server (port 3000)
