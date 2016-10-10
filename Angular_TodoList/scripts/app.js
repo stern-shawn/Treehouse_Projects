64cf9aa0fca39679977af36c1a6ae39d7619a7e7
Shawn Stern <stern.shawn@gmail.com>
2016-10-09 21:27:59 -0700

    Adding a todos array to the controller scope for testing. Using the ng-repeat functionality to iteratively render and create states for each todo item in todos. Changing ng-hide to ng-show so that labels are initially hidden until the user wants to edit.

--- Angular_TodoList/scripts/app.js
+++ Angular_TodoList/scripts/app.js
@@ -3,4 +3,12 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function () {
     console.log("Hello, this is the hellowWorld controller function in mainCtrl!")
   }
+
+  $scope.todos = [
+    {"name": "clean the house"},
+    {"name": "vaccum the rug"},
+    {"name": "don't let the existential dread set in"},
+    {"name": "lift heavy things"},
+    {"name": "eat all the things"}
+  ]
 })
