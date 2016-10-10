2d932ea0f89253d050710388f587083b5e8530e6
Shawn Stern <stern.shawn@gmail.com>
2016-10-09 22:22:58 -0700

    Adding mock Rest API functionality with Angular services. In this case, using the  built-in functionality of Angular to grab json data from a mock server location and assigning it to the value of todos instead of using todos as a hardcoded array inside of the app itself. Practice using the .get() and .then() functions

--- Angular_TodoList/scripts/app.js
+++ Angular_TodoList/scripts/app.js
@@ -1,14 +1,23 @@
 angular.module("todoListApp", [])
-.controller("mainCtrl", function ($scope) {
+.controller("mainCtrl", function ($scope, dataService) {
+  $scope.helloConsole = dataService.helloConsole
+
   $scope.learningNgChange = function () {
     console.log("An input changed!")
   }
 
-  $scope.todos = [
-    {"name": "clean the house"},
-    {"name": "vaccum the rug"},
-    {"name": "don't let the existential dread set in"},
-    {"name": "lift heavy things"},
-    {"name": "eat all the things"}
-  ]
+  dataService.getTodos(function (response) {
+    console.log(response.data)
+    $scope.todos = response.data
+  })
+})
+.service('dataService', function($http) {
+  this.helloConsole = function () {
+    console.log('This is the hello console service')
+  }
+
+  this.getTodos = function(callback) {
+    $http.get('mock/todos.json')
+    .then(callback)
+  }
 })
