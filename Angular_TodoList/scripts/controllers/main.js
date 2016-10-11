dec0cdcd3dac06ee10bf23bff50d517999d7fb08
Shawn Stern <stern.shawn@gmail.com>
2016-10-10 22:21:09 -0700

    Scaffolding the app into separate folders and files to distinguish controller, services, and the main application js files. Adding imports to index.html

---
+++ Angular_TodoList/scripts/controllers/main.js
@@ -0,0 +1,30 @@
+'use strict'
+
+angular.module('todoListApp')
+.controller("mainCtrl", function ($scope, dataService) {
+  $scope.addTodo = function () {
+    var todo = {name: "This is a new todo."}
+    $scope.todos.push(todo)
+  }
+
+  $scope.helloConsole = dataService.helloConsole
+
+  $scope.learningNgChange = function () {
+    console.log("An input changed!")
+  }
+
+  dataService.getTodos(function (response) {
+    console.log(response.data)
+    $scope.todos = response.data
+  })
+
+  $scope.deleteTodo = function (todo, index) {
+    dataService.deleteTodo(todo)
+    // Use the .splice method to remove one element at the given index (delete the todo item)
+    $scope.todos.splice(index, 1)
+  }
+
+  $scope.saveTodo = function (todo) {
+    dataService.saveTodo(todo)
+  }
+})
---
+++ Angular_TodoList/scripts/services/data.js
@@ -0,0 +1,23 @@
+'use strict'
+
+angular.module('todoListApp')
+.service('dataService', function ($http) {
+  this.helloConsole = function () {
+    console.log('This is the hello console service')
+  }
+
+  this.getTodos = function (callback) {
+    $http.get('mock/todos.json')
+    .then(callback)
+  }
+
+  this.deleteTodo = function (todo) {
+    console.log(todo.name + " has been deleted")
+    // Logic to actually remove from the server
+  }
+
+  this.saveTodo = function (todo) {
+    console.log(todo.name + " has been added")
+    // Logic to actually add to the server
+  }
+})
