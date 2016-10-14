b595ef5d52a491259addf44db604d25386572ae4
stern-shawn <stern.shawn@gmail.com>
2016-10-13 22:29:05 -0700

    Adding a mock dataset for future testing with sample todos. Fixing the Angular app, which was deleting the wrong entries if a user selected an item as complete then tried to delete it; element in the same position as the newly checked item would be deleted instead thanks to  not updating on element moves.

--- mean_todo/public/scripts/controllers/todo.js
+++ mean_todo/public/scripts/controllers/todo.js
@@ -2,11 +2,15 @@
 
 angular.module('todoListApp')
 .controller('todoCtrl', function($scope, dataService) {
-  $scope.deleteTodo = function(todo, index) {
-    $scope.todos.splice(index, 1);
-    dataService.deleteTodo(todo);
+  $scope.deleteTodo = function(todo) {
+    // Wrong method from treehouse: use the $index value, which doesn't change
+    // after the todo item is first made...
+    // $scope.todos.splice(index, 1);
+    // Fixed method, use indexOf to get the current location in the todos list
+    $scope.todos.splice($scope.todos.indexOf(todo), 1)
+    dataService.deleteTodo(todo)
   };
-  
+
   $scope.saveTodos = function() {
     var filteredTodos = $scope.todos.filter(function(todo){
       if(todo.edited) {
@@ -14,5 +18,5 @@ angular.module('todoListApp')
       };
     })
     dataService.saveTodos(filteredTodos);
-  }; 
+  };
 });
