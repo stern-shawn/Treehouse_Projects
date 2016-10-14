8df59b3df08cced6cc93db7745af3fece726ad57
stern-shawn <stern.shawn@gmail.com>
2016-10-13 22:00:23 -0700

    Adding a router to the application, making a /todos route to return the todos JSON, and leaving TODO notes for future routes that need adding

--- mean_todo/src/app.js
+++ mean_todo/src/app.js
@@ -6,6 +6,23 @@ var app = express()
 // Serve files in public to the route '/'
 app.use('/', express.static('public'))
 
+var router = express.Router()
+
+// Add a todos route
+router.get('/todos', function (req, res) {
+  res.json({todos: []})
+})
+
+// TODO: Add POST route to create new entries
+
+// TODO: Add PUT route to update existing entries
+
+// TODO: Add DELETE route to delete entries
+
+// Serve the /api namespace using the object router
+// We'll use this namespace to avoid clashes with 'public's' routes
+app.use('/api', router)
+
 // Run server and listen to port 3000
 app.listen(3000, function () {
   console.log('Server is running on port 3000')
