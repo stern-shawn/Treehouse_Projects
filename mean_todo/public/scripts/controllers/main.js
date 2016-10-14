2b48ef62089b78651b4964c7167d5c2d3d083c49
stern-shawn <stern.shawn@gmail.com>
2016-10-13 22:33:20 -0700

    Updating public/services/data.js to point to the /api/todos path and get the todos data from there. Modifying public/controllers/main.js to pull todos data from response.data.todos since the object is formatted slightly differently from the old mockup

--- mean_todo/public/scripts/controllers/main.js
+++ mean_todo/public/scripts/controllers/main.js
@@ -2,15 +2,17 @@
 
 angular.module('todoListApp')
 .controller('mainCtrl', function($scope, dataService){
-  
+
   dataService.getTodos(function(response){
-    var todos = response.data;  
+    // Due to different formatting of the object, todos data is in
+    // response.data.todos instead of just response.data like before
+    var todos = response.data.todos;
     $scope.todos =  todos;
     });
-  
+
   $scope.addTodo = function() {
     $scope.todos.unshift({name: "This is a new todo.",
                       completed: false});
   };
-  
+
 })
--- mean_todo/public/scripts/services/data.js
+++ mean_todo/public/scripts/services/data.js
@@ -3,15 +3,16 @@
 angular.module('todoListApp')
 .service('dataService', function($http) {
   this.getTodos = function(cb) {
-    $http.get('/mock/todos.json').then(cb);
+    // Point this to our /api area
+    $http.get('/api/todos').then(cb);
   };
-  
+
   this.deleteTodo = function(todo) {
     console.log("I deleted the " + todo.name + " todo!");
   };
-  
+
   this.saveTodos = function(todos) {
     console.log("I saved " + todos.length + " todos!");
   };
-  
+
 });
