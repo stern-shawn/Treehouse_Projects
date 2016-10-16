77f89709a7b071ecc0e1229052e9e9202320fc42
stern-shawn <stern.shawn@gmail.com>
2016-10-15 20:49:46 -0700

    Commenting out the practice with  and  since it doesn't contribute to the app

--- mean_todo/app/scripts/controllers/main.js
+++ mean_todo/app/scripts/controllers/main.js
@@ -4,15 +4,15 @@ var angular = require('angular')
 
 angular.module('todoListApp')
 .controller('mainCtrl', function ($scope, $log, $interval, dataService) {
-  $scope.seconds = 0
-
-  $scope.counter = function () {
-    $scope.seconds++
-    $log.log($scope.seconds + ' seconds have passed!')
-  }
-
-  // Syntax: callback to be exectued, delay in ms, # of iterations (optional)
-  $interval($scope.counter, 1000, 10)
+  // $scope.seconds = 0
+  //
+  // $scope.counter = function () {
+  //   $scope.seconds++
+  //   $log.log($scope.seconds + ' seconds have passed!')
+  // }
+  //
+  // // Syntax: callback to be exectued, delay in ms, # of iterations (optional)
+  // $interval($scope.counter, 1000, 10)
 
   dataService.getTodos(function (response) {
     // Due to different formatting of the object, todos data is in
--- mean_todo/public/scripts/todo.bundle.js
+++ mean_todo/public/scripts/todo.bundle.js
@@ -26,15 +26,15 @@ webpackJsonp([0],[
 
 	angular.module('todoListApp')
 	.controller('mainCtrl', function ($scope, $log, $interval, dataService) {
-	  $scope.seconds = 0
-
-	  $scope.counter = function () {
-	    $scope.seconds++
-	    $log.log($scope.seconds + ' seconds have passed!')
-	  }
-
-	  // Syntax: callback to be exectued, delay in ms, # of iterations (optional)
-	  $interval($scope.counter, 1000, 10)
+	  // $scope.seconds = 0
+	  //
+	  // $scope.counter = function () {
+	  //   $scope.seconds++
+	  //   $log.log($scope.seconds + ' seconds have passed!')
+	  // }
+	  //
+	  // // Syntax: callback to be exectued, delay in ms, # of iterations (optional)
+	  // $interval($scope.counter, 1000, 10)
 
 	  dataService.getTodos(function (response) {
 	    // Due to different formatting of the object, todos data is in
