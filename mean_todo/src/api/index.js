eeeec2b718ab9bd6ebcf73165ea86c6988049c1e
stern-shawn <stern.shawn@gmail.com>
2016-10-15 19:19:59 -0700

    Attaching PUT route to /todos so user can update data for a given id

--- mean_todo/src/api/index.js
+++ mean_todo/src/api/index.js
@@ -28,7 +28,23 @@ router.post('/todos', function (req, res) {
     res.json({'todo': todo, message: 'Todo created'})
   })
 })
-// TODO: Add PUT route to update existing entries
+
+// PUT route for updating todos
+router.put('/todos/:id', function (req, res) {
+  var id = req.params.id
+  var todo = req.body
+
+  if (todo && todo._id !== id) {
+    return res.status(500).json({err: 'Ids don\'t match'})
+  }
+  // Syntax: id to look up entry, new data for entry, (optional options), callback
+  Todo.findByIdAndUpdate(id, todo, {new: true}, function (err, todo) {
+    if (err) {
+      return res.status(500).json({err: err.message})
+    }
+    res.json({'todo': todo, message: 'Todo updated'})
+  })
+})
 
 // TODO: Add DELETE route to delete entries
 
