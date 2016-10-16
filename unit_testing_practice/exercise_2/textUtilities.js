4e59e81494134fb579c2b5c3f5fbb3d7eda03674
stern-shawn <stern.shawn@gmail.com>
2016-10-16 13:02:13 -0700

    Building up the testing and functionality of tested function starting from basic to more complex expectations. ie check if we get the right output format, can capitalize one letter, then one word, then a sequence of words. Next step is even more complex expectations like puncation, etc

--- unit_testing_practice/exercise_2/textUtilities.js
+++ unit_testing_practice/exercise_2/textUtilities.js
@@ -2,8 +2,20 @@ var expect = require('chai').expect
 
 expect(true).to.be.true
 
-function titleCase(title) {
-  return title
+function titleCase (title) {
+  var words = title.split(' ')
+
+  // Apply the capitalize first letter, return remainder of word logic to
+  // the entire title
+  var titleCasedWords = words.map(function (word) {
+    return word[0].toUpperCase() + word.substring(1)
+  })
+
+  return titleCasedWords.join(' ')
 }
 
 expect(titleCase('the great mouse detective')).to.be.a('string')
+expect(titleCase('a')).to.equal('A')
+expect(titleCase('vertigo')).to.equal('Vertigo')
+
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective')
