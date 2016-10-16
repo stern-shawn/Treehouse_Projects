b23fa682261bfbadd6976b41db4836d63f9d52e8
stern-shawn <stern.shawn@gmail.com>
2016-10-15 18:45:49 -0700

    Setting up api/todos route with MongoDB connection instead of the mock data. Establishing error code 500 and message when there is an error.

--- mean_todo/src/api/index.js
+++ mean_todo/src/api/index.js
@@ -1,13 +1,21 @@
 'use strict'
 
 var express = require('express')
-var todos = require('../../mock/todos.json')
+// Import Todo model instead of using mock data
+var Todo = require('../models/todo')
+// var todos = require('../../mock/todos.json')
 
 var router = express.Router()
 
 // Add a todos route
 router.get('/todos', function (req, res) {
-  res.json({todos: todos})
+  Todo.find({}, function (err, todos) {
+    if (err) {
+      return res.status(500).json({message: err.message})
+      // return console.log(err)
+    }
+    res.json({todos: todos})
+  })
 })
 
 // TODO: Add POST route to create new entries
