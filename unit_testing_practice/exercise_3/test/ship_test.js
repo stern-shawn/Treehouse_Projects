5c4b6a7cd60146d49bc949c113131bc71acb1ff3
stern-shawn <stern.shawn@gmail.com>
2016-10-19 15:46:13 -0700

    Importing player_methods and player_tests from Treehouse and adding in some tests for edge cases, such as if a user tries to call a function with missing params, etc.

--- unit_testing_practice/exercise_3/test/ship_test.js
+++ unit_testing_practice/exercise_3/test/ship_test.js
@@ -1,106 +1,108 @@
 var expect = require('chai').expect
 
-describe('checkForShip', function () {
-  var checkForShip = require('../game_logic/ship_methods').checkForShip
-  var player
+describe('SHIP METHODS', function () {
+  describe('checkForShip', function () {
+    var checkForShip = require('../game_logic/ship_methods').checkForShip
+    var player
 
-  // Factor out the most complex player object from all tests and make it
-  // available to all using the Mocha before() method.
-  // This is possible since checkForShip doesn't modify player
-  before(function () {
-    player = {
-      ships: [
-        {
-          locations: [[0, 0], [0, 1]]
-        },
-        {
-          locations: [[1, 0], [1, 1]]
-        },
-        {
-          locations: [[2, 0], [2, 1], [2, 2], [2, 3]]
-        }
-      ]
-    }
+    // Factor out the most complex player object from all tests and make it
+    // available to all using the Mocha before() method.
+    // This is possible since checkForShip doesn't modify player
+    before(function () {
+      player = {
+        ships: [
+          {
+            locations: [[0, 0], [0, 1]]
+          },
+          {
+            locations: [[1, 0], [1, 1]]
+          },
+          {
+            locations: [[2, 0], [2, 1], [2, 2], [2, 3]]
+          }
+        ]
+      }
+    })
+
+    it('should correctly report no ship at a given players coordinate', function () {
+      expect(checkForShip(player, [9, 9])).to.be.false
+    })
+
+    it('should correctly report a ship at the given coordinates', function () {
+      expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
+    })
+
+    it('should handle ships located at more than one coordinate', function () {
+      expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
+      expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0])
+      expect(checkForShip(player, [9, 9])).to.be.false
+    })
+
+    it('should handle checking multiple ships', function () {
+      expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
+      expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0])
+
+      expect(checkForShip(player, [1, 0])).to.deep.equal(player.ships[1])
+      expect(checkForShip(player, [1, 1])).to.deep.equal(player.ships[1])
+
+      expect(checkForShip(player, [2, 3])).to.deep.equal(player.ships[2])
+
+      expect(checkForShip(player, [9, 9])).to.be.false
+    })
   })
 
-  it('should correctly report no ship at a given players coordinate', function () {
-    expect(checkForShip(player, [9, 9])).to.be.false
+  describe('damageShip', function () {
+    var damageShip = require('../game_logic/ship_methods').damageShip
+
+    it('should register damage on a given ship at a given location', function () {
+      var ship = {
+        locations: [[0,0]],
+        damage: []
+      }
+
+      damageShip(ship, [0,0])
+
+      expect(ship.damage).to.not.be.empty
+      expect(ship.damage[0]).to.deep.equal([0,0])
+
+    })
   })
 
-  it('should correctly report a ship at the given coordinates', function () {
-    expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
-  })
+  describe('fire', function () {
+    var fire = require('../game_logic/ship_methods').fire
+    var player
 
-  it('should handle ships located at more than one coordinate', function () {
-    expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
-    expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0])
-    expect(checkForShip(player, [9, 9])).to.be.false
-  })
+    // Similarly, since fire does modify the player object, we can use beforeEach()
+    // to create a fresh copy of player for each test to make use of
+    beforeEach(function () {
+      player = {
+        ships: [
+          {
+            locations: [[0, 0]],
+            damage: []
+          }
+        ]
+      }
+    })
 
-  it('should handle checking multiple ships', function () {
-    expect(checkForShip(player, [0, 0])).to.deep.equal(player.ships[0])
-    expect(checkForShip(player, [0, 1])).to.deep.equal(player.ships[0])
+    // Log completion of the test suite
+    after(function () {
+      console.log('Test suite completed')
+    })
 
-    expect(checkForShip(player, [1, 0])).to.deep.equal(player.ships[1])
-    expect(checkForShip(player, [1, 1])).to.deep.equal(player.ships[1])
+    // Only for example
+    // afterEach(function () {
+    //   console.log('one unit test completed...')
+    // })
 
-    expect(checkForShip(player, [2, 3])).to.deep.equal(player.ships[2])
+    it('should record damage on given ship at a given coordinate', function () {
+      fire(player, [0, 0])
+      expect(player.ships[0].damage[0]).to.deep.equal([0,0])
+    })
 
-    expect(checkForShip(player, [9, 9])).to.be.false
-  })
-})
-
-describe('damageShip', function () {
-  var damageShip = require('../game_logic/ship_methods').damageShip
-
-  it('should register damage on a given ship at a given location', function () {
-    var ship = {
-      locations: [[0,0]],
-      damage: []
-    }
-
-    damageShip(ship, [0,0])
-
-    expect(ship.damage).to.not.be.empty
-    expect(ship.damage[0]).to.deep.equal([0,0])
-
-  })
-})
-
-describe('fire', function () {
-  var fire = require('../game_logic/ship_methods').fire
-  var player
-
-  // Similarly, since fire does modify the player object, we can use beforeEach()
-  // to create a fresh copy of player for each test to make use of
-  beforeEach(function () {
-    player = {
-      ships: [
-        {
-          locations: [[0, 0]],
-          damage: []
-        }
-      ]
-    }
-  })
-
-  // Log completion of the test suite
-  after(function () {
-    console.log('Test suite completed')
-  })
-
-  // Only for example
-  // afterEach(function () {
-  //   console.log('one unit test completed...')
-  // })
-
-  it('should record damage on given ship at a given coordinate', function () {
-    fire(player, [0, 0])
-    expect(player.ships[0].damage[0]).to.deep.equal([0,0])
-  })
-
-  it('should not record damage if no ship at given coordinates', function () {
-    fire(player, [9, 9])
-    expect(player.ships[0].damage).to.be.empty
+    it('should not record damage if no ship at given coordinates', function () {
+      fire(player, [9, 9])
+      expect(player.ships[0].damage).to.be.empty
+    })
   })
 })
