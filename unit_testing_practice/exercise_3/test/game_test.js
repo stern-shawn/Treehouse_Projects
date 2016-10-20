1353c8eaa34114e2fb64dc96c91452f94cbb83d3
stern-shawn <stern.shawn@gmail.com>
2016-10-20 15:50:50 -0700

    Adding a test:watch command to npm which uses mocha --watch to retest the suite whenever any files change for live test feedback

--- unit_testing_practice/exercise_3/test/game_test.js
+++
@@ -1,7 +0,0 @@
-var expect = require('chai').expect
-
-describe('GAME INSTANCE FUNCTIONS', function () {
-  describe('checkGameStatus', function () {
-    it('should tell me when the game is over')
-  })
-})
